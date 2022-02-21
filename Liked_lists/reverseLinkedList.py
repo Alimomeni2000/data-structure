96de6e3ba94817ec3ed412a02d866a33db8850c8
ali momeni <alimomeni2000.official@gmail.com>
2022-02-21 12:23:16 +0330

    reverse linked list

---
+++ Liked_lists/reverseLinkedList.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+a= Node('A')
+b= Node('B')
+c= Node('C')
+d= Node('D')
+e= Node('E')
+
+a.next = b 
+b.next = c
+c.next = d
+d.next = e
+
+# def reverseList(head):
+
+#     prev = None
+#     current =head
+#     while current !=None:
+#         next= current.next
+#         current.next = prev
+#         prev = current
+#         current = next
+#     return prev
+def reverseList(head,prev = None):
+    if head ==None:
+        return prev
+    next = head.next
+    head.next = prev
+    return reverseList(next, head)
+
+
+print(reverseList(a).val)
+new_head =reverseList(a)
+print(reverseList(new_head).val)
+
